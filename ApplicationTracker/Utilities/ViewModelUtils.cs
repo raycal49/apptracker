e78a0f6e51b85c08743e7a59ede51c7fe77a431e
Raymond Calderon <raymondcal49@gmail.com>
2024-08-14 21:38:42 -0500

    Add ViewModelUtils.cs to Utilities folder

---
+++ ApplicationTracker/Utilities/ViewModelUtils.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Utilities
+{
+    public class ViewModelUtils
+    {
+        //
+        public static IEnumerable<ProcessWrapper> ConvertProcDataToProcWrapper(IEnumerable<ProcessData> unconverted)
+        {
+            var converted = new List<ProcessWrapper>();
+
+            foreach (var proc in unconverted)
+            {
+                converted.Add(new ProcessWrapper { ProcessName = proc.ProcessName, ProcessTime = proc.ProcessTime, ProcessDate = proc.ProcessDate });
+            }
+
+            return converted;
+        }
+    }
+}
