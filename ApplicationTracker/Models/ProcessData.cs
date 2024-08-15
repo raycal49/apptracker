56a81ed7b5fcf2757bd9a2cf0e9e13f2238547b0
Raymond Calderon <raymondcal49@gmail.com>
2024-08-14 21:24:15 -0500

    Add ProcessData to Models folder

---
+++ ApplicationTracker/Models/ProcessData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Models
+{
+    public class ProcessData
+    {
+        public int Id { get; set; }
+        public required string ProcessName { get; set; }
+        public TimeSpan ProcessTime { get; set; }
+        public DateTime ProcessDate { get; set; }
+    }
+}
--- ApplicationTracker/obj/Debug/net7.0-windows/ApplicationTracker.AssemblyInfo.cs
+++ ApplicationTracker/obj/Debug/net7.0-windows/ApplicationTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09daf8096ee46c2f7b4c3b246459e46abb4ffd54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf2d3bac3b6c90bd9e0c674c3176fa6dcb5b439")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
