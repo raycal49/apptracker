de01713528f928a1bd4d9585e08d7ecaaafdbff0
Raymond Calderon <raymondcal49@gmail.com>
2024-08-15 14:50:25 -0500

    Add WeekViewModel.cs to View Model folder and add tab for it to MainWindow.xaml

---
+++ ApplicationTracker/View Models/WeekViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.View_Models
+{
+    public class WeekViewModel
+    {
+        public ObservableCollection<ProcessWrapper>? WeeklyTotal { get; set; }
+    }
+}
