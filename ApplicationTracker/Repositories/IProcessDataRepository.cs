f3a33ab0f02a4b8a012424b95e1c9826f6764641
Raymond Calderon <raymondcal49@gmail.com>
2024-08-14 21:30:14 -0500

    Add IProcessDataRepository.cs and ProcessDataRepository.cs to Repositories folder

---
+++ ApplicationTracker/Repositories/IProcessDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Repositories
+{
+    public interface IProcessDataRepository : IRepository<ProcessData>
+    {
+        ProcessData? FindFirstOrDef(Expression<Func<ProcessData, bool>> predicate);
+    }
+}
---
+++ ApplicationTracker/Repositories/ProcessDataRepository.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker.Repositories
+{
+    public class ProcessDataRepository : Repository<ProcessData>, IProcessDataRepository
+    {
+        public ProcessDataRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public new IEnumerable<ProcessData> Find(Expression<Func<ProcessData, bool>> predicate)
+        {
+            //return Context.Set<ProcessTableEntry>().Where(predicate).ToList();
+            return Context.Set<ProcessData>().Where(predicate).ToList();
+        }
+
+        ProcessData? IProcessDataRepository.FindFirstOrDef(Expression<Func<ProcessData, bool>> predicate)
+        {
+            return Context.Set<ProcessData>().FirstOrDefault(predicate);
+        }
+    }
+}
